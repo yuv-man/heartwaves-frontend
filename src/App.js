a9b0e6a508a44622611bbb7bf89b04af0c52bd37
yuval-mandler <yuvalmandler@gmail.com>
2021-01-20 11:24:37 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import HomePage from './components/homePage';
 
 function App() {
   return (
     <div className="App">
+      <HomePage/>
     </div>
   );
 }
