1e5aff63321f24e398ba95b0916b4111f4dc196c
yuval-mandler <yuvalmandler@gmail.com>
2021-01-21 11:19:24 +0200

    signup

---
+++ src/libs/util.js
@@ -0,0 +1,23 @@
+const BASE_URL = 'http://localhost:3001'
+
+const signup = async( firstName, lastName, email, phoneNumber, password) =>{
+    const response = await fetch(`${BASE_URL}/api/user/sign-up`,{
+        method: 'POST',
+        credentials: "include",
+        headers:{
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ firstName, lastName, email, phoneNumber, password })
+    })
+    let returnResponse
+        returnResponse = response.text()
+        const result = await returnResponse
+    if (!response.ok){
+        return result
+    } else {
+        const userId = [result]
+        return userId
+    }
+}
+
+export { signup }
